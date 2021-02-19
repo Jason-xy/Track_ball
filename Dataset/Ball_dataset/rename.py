0062c5d33bf82dc68275f7f9117eca2dd9b07a80
jason-xy <1462357003@qq.com>
2021-02-19 23:58:46 +0800

    First Training

--- Dataset/Ball_dataset/rename.py
+++
@@ -1,8 +0,0 @@
-#coding=utf-8  
-  
-import os
-file_path = os.path.abspath('.')
-count = 501
-for file in os.listdir(file_path):
-    os.rename(os.path.join(file_path,file),os.path.join(file_path,str(count)+".jpg"))
-    count+=1
