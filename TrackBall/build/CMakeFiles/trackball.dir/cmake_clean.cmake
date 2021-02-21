dcd03692eac5b45de88d003fa77ba8df0bf5e72e
jason-xy <1462357003@qq.com>
2021-02-21 11:49:14 +0800

    Target detection module completed

---
+++ TrackBall/build/CMakeFiles/trackball.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trackball.dir/trackball.cpp.o"
+  "trackball.pdb"
+  "trackball"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/trackball.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
